fd78ec242ae3e0a3122da5552102b2b0846919b8
cindyizza13@gmail.com <cindyizza13@gmail.com>
2025-05-04 18:30:31 +0700

    part5-15

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::create([
+            'nama' => 'Alif',
+            'email' => 'admin@gmail.com',
+            'jabatan' => 'Admin',
+            'password' => Hash::make('123123123'),
+            'is_tugas' => false,
+        ]);
+        User::create([
+            'nama' => 'cindy',
+            'email' => 'cindy@gmail.com',
+            'jabatan' => 'karyawan',
+            'password' => Hash::make('123123123'),
+            'is_tugas' => false,
+        ]);
+        User::create([
+            'nama' => 'diego',
+            'email' => 'diego@gmail.com',
+            'jabatan' => 'karyawan',
+            'password' => Hash::make('123123123'),
+            'is_tugas' => false,
+        ]);
+
+        User::create([
+            'nama' => 'Tini',
+            'email' => 'tini@gmail.com',
+            'jabatan' => 'karyawan',
+            'password' => Hash::make('123123123'),
+            'is_tugas' => false,
+        ]);
+    }
+}
